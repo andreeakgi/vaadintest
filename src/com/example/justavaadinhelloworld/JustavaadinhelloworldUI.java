b5e863c3ecc4870908517f164c7f2409d0e56fc5
Andreea <andreea.kgi@gmail.com>
2014-06-04 00:46:48 +0200

    Modified to combobox

--- src/com/example/justavaadinhelloworld/JustavaadinhelloworldUI.java
+++ src/com/example/justavaadinhelloworld/JustavaadinhelloworldUI.java
@@ -1,13 +1,17 @@
 package com.example.justavaadinhelloworld;
 
+import java.io.File;
+
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.util.FilesystemContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -21,21 +25,12 @@ public class JustavaadinhelloworldUI extends UI {
 	public static class Servlet extends VaadinServlet {
 	}
 
+	
+	FilesystemContainer docs= new FilesystemContainer(new File("/Downloads"));
+	ComboBox docList = new ComboBox("Documents", docs);
 	@Override
 	protected void init(VaadinRequest request) {
-		final VerticalLayout layout = new VerticalLayout();
-		layout.setMargin(true);
-		setContent(layout);
-
-		Button button = new Button("Click Me!");
-		button.setHeight("100");
-		button.setDescription("im really a button");
-		button.addClickListener(new Button.ClickListener() {
-			public void buttonClick(ClickEvent event) {
-				layout.addComponent(new Label("Thank you for clicking"));
-			}
-		});
-		layout.addComponent(button);
+	setContent(docList);
 	}
 
 }
