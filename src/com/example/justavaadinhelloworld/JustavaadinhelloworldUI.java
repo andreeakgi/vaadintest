38d774bec6dfef723f74e0d589d661d602467a0f
Andreea <andreea.kgi@gmail.com>
2014-06-04 13:44:17 +0200

    split and we can see the files

--- src/com/example/justavaadinhelloworld/JustavaadinhelloworldUI.java
+++ src/com/example/justavaadinhelloworld/JustavaadinhelloworldUI.java
@@ -6,12 +6,17 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.data.util.FilesystemContainer;
+import com.vaadin.data.util.TextFileProperty;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -28,9 +33,27 @@ public class JustavaadinhelloworldUI extends UI {
 	// finally figured out how the path must be set
 	FilesystemContainer docs= new FilesystemContainer(new File("/tmp/docs"));
 	ComboBox docList = new ComboBox("Documents", docs);
+	Label docView = new Label("",ContentMode.HTML);
+	
 	@Override
 	protected void init(VaadinRequest request) {
-	setContent(docList);
+	
+		HorizontalSplitPanel split = new HorizontalSplitPanel();
+		setContent(split);
+		split.addComponent(docList);
+		split.addComponent(docView);
+		
+		docList.addValueChangeListener(new ValueChangeListener() {
+			
+			
+			public void valueChange(ValueChangeEvent event) {
+			    docView.setPropertyDataSource(new TextFileProperty((File) event.getProperty().getValue()));	
+				
+			}
+		});
+		
+		docList.setImmediate(true);
 	}
+	
 
 }
